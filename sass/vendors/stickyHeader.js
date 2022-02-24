98d31e20b703699cd608275b9921feef9a60eb3b
turalcavadn <turaltj@code.edu.az>
2022-02-24 21:23:57 +0400

    functional sticky navbar

--- sass/vendors/stickyHeader.js
+++ sass/vendors/stickyHeader.js
@@ -1,16 +1,31 @@
-const firstSectionText = document.querySelector(".special-offer-section");
+const firstSectionText = document.querySelector(".our-menu-btn-wrapper");
 
 const initialCoords = firstSectionText.getBoundingClientRect();
 
+const headerNavTags = document.querySelectorAll(".header-nav-tags");
+
+const socialMediaButtons = document.querySelectorAll(".social-media-buttons");
+const logo = document.querySelector(".logo");
+const logoSticky = document.querySelector(".logo-sticky");
 const header = document.querySelector(".header-content-wrapper");
 
 window.addEventListener("scroll", function () {
-	console.log(header);
 	console.log(initialCoords.top);
-	if (window.scrollY > initialCoords.top) {
-		console.log("test");
+	if (window.scrollY > initialCoords.top && window.innerWidth > 991) {
 		header.classList.add("sticky");
+		logo.style.display = "none";
+		logoSticky.style.display = "block";
+		headerNavTags.forEach((tag) => {
+			tag.classList.add("sticky-navbar");
+		});
+		socialMediaButtons.forEach((buttons) => (buttons.style.color = "#333"));
 	} else {
 		header.classList.remove("sticky");
+		headerNavTags.forEach((tag) => {
+			tag.classList.remove("sticky-navbar");
+		});
+		socialMediaButtons.forEach((buttons) => (buttons.style.color = "#fff"));
+		logo.style.display = "block";
+		logoSticky.style.display = "none";
 	}
 });
